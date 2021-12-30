8093fb3a677ede381609f83297f9c4e7c0a42b68
meohin <meohin.meo@gmail.com>
2021-12-30 18:13:39 +0900

    Add MethodSignature

--- Part3/src/chapter5/src/main/java/springIntro/aop/aop/ParameterAop.java
+++ Part3/src/chapter5/src/main/java/springIntro/aop/aop/ParameterAop.java
@@ -2,18 +2,25 @@ package springIntro.aop.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 @Aspect     // aop 로 동작한다.
 @Component  // spring 에서 Component 로 관리한다.
 public class ParameterAop {
-
     @Pointcut("execution(* com.example.aop.controller..*.*(..))")
     private  void cut() {}
 
 //    메소드가 실행되기 전에 넘어가는 arg 확인
     @Before("cut()")
     public void before(JoinPoint joinPoint) {
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName());
+
         Object[] args = joinPoint.getArgs();
         for (Object obj : args) {
             System.out.println("type: " + obj.getClass().getSimpleName());
--- Part3/src/chapter5/src/main/java/springIntro/aop/controller/RestApiController.java
+++ Part3/src/chapter5/src/main/java/springIntro/aop/controller/RestApiController.java
@@ -9,16 +9,16 @@ public class RestApiController {
 
     @GetMapping("/get/{id}")
     public String get(@PathVariable Long id, @RequestParam String name){
-        System.out.println("get method");
-        System.out.println("get method: " + id);
-        System.out.println("get method: " + name);
+//        System.out.println("get method");
+//        System.out.println("get method: " + id);
+//        System.out.println("get method: " + name);
         return id + " " + name;
 
     }
 
     @PostMapping("/post")
     public User post(@RequestBody User user){
-        System.out.println("post method" + user);
+//        System.out.println("post method" + user);
         return user;
     }
 }
