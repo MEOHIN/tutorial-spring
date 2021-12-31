9d019f05b186c45409d7421752bb5d736bd8e93c
meohin <meohin.meo@gmail.com>
2021-12-31 16:45:40 +0900

    type: Start StopWatch"

--- Part3/src/chapter5/src/main/java/springIntro/aop/aop/TimerAop.java
+++ Part3/src/chapter5/src/main/java/springIntro/aop/aop/TimerAop.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
@@ -17,7 +18,9 @@ public class TimerAop {
     private void enableTimer(){}
 
     @Around("cut() && enableTime()")
-    public void around(ProceedingJoinPoint joinPoint) {
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
 
         Object result = joinPoint.proceed();
 
