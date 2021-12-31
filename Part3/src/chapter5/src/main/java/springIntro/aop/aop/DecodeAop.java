20dc4073a32c496d5685926f03dbaeb627dc1429
meohin <meohin.meo@gmail.com>
2021-12-31 17:48:28 +0900

    type: Add method with pointcut annotation

--- Part3/src/chapter5/src/main/java/springIntro/aop/aop/DecodeAop.java
+++ Part3/src/chapter5/src/main/java/springIntro/aop/aop/DecodeAop.java
@@ -1,4 +1,16 @@
 package springIntro.aop.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class DecodeAop {
+
+    @Pointcut("execution(* springIntro.aop.controller..*.*(..))")
+    private void cut(){}
+
+    @Pointcut("@annotation(springIntro.aop.annotation.Decode)")
+    private void enableDecode(){}
 }
