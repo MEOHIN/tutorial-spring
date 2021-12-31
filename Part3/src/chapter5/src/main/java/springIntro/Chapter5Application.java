f763b193ba66169c2cfb0440ef403a6a158785eb
meohin <meohin.meo@gmail.com>
2021-12-31 18:00:05 +0900

    type: Print result

--- Part3/src/chapter5/src/main/java/springIntro/Chapter5Application.java
+++ Part3/src/chapter5/src/main/java/springIntro/Chapter5Application.java
@@ -3,11 +3,14 @@ package springIntro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Base64;
+
 @SpringBootApplication
 public class Chapter5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter5Application.class, args);
+		System.out.println(Base64.getEncoder().encodeToString("steve@gmail.com".getBytes()));
 	}
 
 }
