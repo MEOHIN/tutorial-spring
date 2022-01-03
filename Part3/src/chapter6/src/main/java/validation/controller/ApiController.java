1280ed23df3bca188c01a67662b04b1e2ce3a137
meohin <meohin.meo@gmail.com>
2022-01-03 17:27:15 +0900

    Make validate input value type

--- Part3/src/chapter6/src/main/java/validation/controller/ApiController.java
+++ Part3/src/chapter6/src/main/java/validation/controller/ApiController.java
@@ -1,17 +1,35 @@
 package validation.controller;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import validation.dto.User;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     @PostMapping("/user")
-    public Object user(@RequestBody User user) {
+//    @Valid: 해당 어노테이션이 붙은 객체는 그 객체 안에 있는 어노테이션을 검사하고 값을 검증
+//    BindingResult: validation 의 결과가 할
+    public Object user(@Valid @RequestBody User user, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("field: " + field.getField());
+                System.out.println(message);
+            });
+        }
+
         System.out.println(user);
         return user;
     };
