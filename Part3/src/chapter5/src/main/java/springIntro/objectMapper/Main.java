86219a5fef2e77e1e86426f48f9b60ed32ef784f
meohin <meohin.meo@gmail.com>
2022-01-03 11:25:32 +0900

    type: Create object

--- Part3/src/chapter5/src/main/java/springIntro/objectMapper/Main.java
+++ Part3/src/chapter5/src/main/java/springIntro/objectMapper/Main.java
@@ -1,7 +1,35 @@
 package springIntro.objectMapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import springIntro.objectMapper.dto.Car;
+import springIntro.objectMapper.dto.User;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("main");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User user = new User();
+        user.setName("홍길동");
+        user.setAge(10);
+
+        Car car1 = new Car();
+        car1.setName("K5");
+        car1.setCarNumber("11가 1111");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("Q5");
+        car2.setCarNumber("22가 2222");
+        car2.setType("SUV");
+
+//        car1, car2 를 리스트로 묶기
+        List<Car> carList = Arrays.asList(car1, car2);
+        user.setCar(carList);
+
+        System.out.println(user);
     }
 }
