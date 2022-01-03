cfabad93ed2c7f9bb7fc99d6e8ebd0aa813445c1
meohin <meohin.meo@gmail.com>
2022-01-03 17:01:46 +0900

    Create test project for springboot

--- Part3/src/chapter6/src/main/java/validation/controller/ApiController.java
+++ Part3/src/chapter6/src/main/java/validation/controller/ApiController.java
@@ -1,2 +1,18 @@
-package validation.controller;public class ApiController {
+package validation.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import validation.dto.User;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/user")
+    public Object user(@RequestBody User user) {
+        System.out.println(user);
+        return user;
+    };
 }
