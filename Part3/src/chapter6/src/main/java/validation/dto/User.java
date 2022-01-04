3b09a3cccd16cacf579a7d2203dcfb7a63d822fb
meohin <meohin.meo@gmail.com>
2022-01-04 16:03:54 +0900

    Correct user object

--- Part3/src/chapter6/src/main/java/validation/dto/User.java
+++ Part3/src/chapter6/src/main/java/validation/dto/User.java
@@ -7,20 +7,21 @@ import java.util.List;
 
 public class User {
 
+    @NotBlank
     private String name;
 
     @Max(value = 90)
     private int age;
 
-    @Email
-    private String email;
-
-
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")     // 핸드폰 번호 정규식 "^\\d{2,3}-\\d{3,4}-\\d{4}$"
-    private String phoneNumber;
-
-    @YearMonth
-    private String reqYearMonth;        //  yyyyMM  요청하는 시간
+//    @Email
+//    private String email;
+//
+//
+//    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")     // 핸드폰 번호 정규식 "^\\d{2,3}-\\d{3,4}-\\d{4}$"
+//    private String phoneNumber;
+//
+//    @YearMonth
+//    private String reqYearMonth;        //  yyyyMM  요청하는 시간
 
     private List<Car> cars;
 
@@ -40,29 +41,29 @@ public class User {
         this.age = age;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getReqYearMonth() {
-        return reqYearMonth;
-    }
-
-    public void setReqYearMonth(String reqYearMonth) {
-        this.reqYearMonth = reqYearMonth;
-    }
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//
+//    public String getReqYearMonth() {
+//        return reqYearMonth;
+//    }
+//
+//    public void setReqYearMonth(String reqYearMonth) {
+//        this.reqYearMonth = reqYearMonth;
+//    }
 
     public List<Car> getCars() {
         return cars;
@@ -93,9 +94,9 @@ public class User {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", reqYearMonth='" + reqYearMonth + '\'' +
+//                ", email='" + email + '\'' +
+//                ", phoneNumber='" + phoneNumber + '\'' +
+//                ", reqYearMonth='" + reqYearMonth + '\'' +
                 ", cars=" + cars +
                 '}';
     }
