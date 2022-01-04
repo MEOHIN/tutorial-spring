eb32042fa323e124977d10e8e0e6ab9926c25897
meohin <meohin.meo@gmail.com>
2022-01-04 15:26:34 +0900

    Create customized annotation

---
+++ Part3/src/chapter6/src/main/java/validation/annotation/YaerMonth.java
@@ -0,0 +1,22 @@
+package validation.annotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Constraint(validatedBy = { })
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+public @interface YaerMonth {
+
+    String message() default "{javax.validation.constraints.Email.message}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
