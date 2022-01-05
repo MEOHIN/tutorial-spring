5d067c3278053b0c6641e2a2cf1110fc755bd9be
meohin <meohin.meo@gmail.com>
2022-01-05 17:24:15 +0900

    Correct annotation

--- Part3/src/chapter6/src/main/java/validation/exception/advice/GlobalControllerAdvice.java
+++ Part3/src/chapter6/src/main/java/validation/exception/advice/GlobalControllerAdvice.java
@@ -3,9 +3,9 @@ package validation.exception.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = Exception.class)
