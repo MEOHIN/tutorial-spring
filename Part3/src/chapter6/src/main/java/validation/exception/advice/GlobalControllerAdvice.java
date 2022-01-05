02161c223fad5567f2dac99ea72906f8c315159f
meohin <meohin.meo@gmail.com>
2022-01-05 17:20:34 +0900

    Implement global controller advice object

---
+++ Part3/src/chapter6/src/main/java/validation/exception/advice/GlobalControllerAdvice.java
@@ -0,0 +1,18 @@
+package validation.exception.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity exception (Exception e){
+        System.out.println("--------------------");
+        System.out.println(e.getLocalizedMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+}
