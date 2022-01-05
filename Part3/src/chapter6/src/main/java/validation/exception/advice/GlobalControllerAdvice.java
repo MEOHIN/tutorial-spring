3451150ad8c6ab089541d5f12ff9f3c702436855
meohin <meohin.meo@gmail.com>
2022-01-05 17:29:57 +0900

    Improve response quality

--- Part3/src/chapter6/src/main/java/validation/exception/advice/GlobalControllerAdvice.java
+++ Part3/src/chapter6/src/main/java/validation/exception/advice/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package validation.exception.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity exception (Exception e){
+    public ResponseEntity exception(Exception e){
+        System.out.println(e.getClass().getName());     // MethodArgumentNotValidException : 해당 클래스가 어디에 잘못된 예외인지 출력
         System.out.println("--------------------");
         System.out.println(e.getLocalizedMessage());
+        System.out.println("--------------------");
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
