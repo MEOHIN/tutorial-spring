6aa1437f53f00152b4faa301049378f1bb249f9c
meohin <meohin.meo@gmail.com>
2022-01-06 15:04:32 +0900

    Implement RestAPI controller and User object

--- Part3/src/chapter6/src/main/java/validation/filter/Filter.md
+++ Part3/src/chapter6/src/main/java/validation/filter/Filter.md
@@ -4,9 +4,15 @@
 - 요청/응답의 정보를 변경
 - Spring 에 의해서 데이터가 변환되기 전의 순수한 Client 의 요청(request body)/응답(response body) 값을 확인
 
-**유일하게 SevletResponse 의 객체를 변환**할 수 있다.
+**유일하게 ServletResponse 의 객체를 변환**할 수 있다.
 
 ### Filter 주 활용
 - Spring Framework 에서는 request /response 의 Logic 용도로 활용
 - 인증과 관련된 Logic 들을 처리
-- 선/후 처리 함으로써, Service business logic 과 분리
+- 선/후 처리 함으로써, Service business logic 과 분 
+
+### Spring MVC request life cycle
+Request 들어옴 -> Filter -> DispatchServlet -> HandlerInterceptor -> AOP 동작 -> HandlerInterceptor -> DispatchServlet -> Filter -> Response 보냄
+
+## sample code
+- Filter 에서 client 가 보낸 request body 가 어떤 것이 들어있는지 기록
---
+++ Part3/src/chapter6/src/main/java/validation/filter/controller/ApiController.java
@@ -0,0 +1,21 @@
+package validation.filter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import validation.filter.dto.User;
+
+@Slf4j      // System Log for Java: system.out 대신 log 를 찍을 수 있음
+@RestController
+@RequestMapping("/api/user3")
+public class ApiController {
+
+    @PostMapping("")
+    public User user (@RequestBody User user) {
+        log.info("User: {}" , user);
+
+        return user;
+    }
+}
