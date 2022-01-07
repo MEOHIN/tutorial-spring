89eedd4c04a41613d9fc9db7e2dc8b1de1465595
meohin <meohin.meo@gmail.com>
2022-01-07 17:04:44 +0900

    Add customized annotation

--- Part3/src/chapter6/src/main/java/validation/interceptor/controller/PrivateController.java
+++ Part3/src/chapter6/src/main/java/validation/interceptor/controller/PrivateController.java
@@ -3,9 +3,11 @@ package validation.interceptor.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import validation.interceptor.annotation.Auth;
 
 @RestController
 @RequestMapping("/api/private")
+@Auth
 public class PrivateController {
 
     @GetMapping("/hello")
