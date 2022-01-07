06835238f684e0560fe5ab051048e136522aa6a9
meohin <meohin.meo@gmail.com>
2022-01-07 18:50:37 +0900

    Make Handler get exception

--- Part3/src/chapter6/src/main/java/validation/interceptor/handler/GlobalExceptionHandler.java
+++ Part3/src/chapter6/src/main/java/validation/interceptor/handler/GlobalExceptionHandler.java
@@ -1,2 +1,17 @@
-package validation.interceptor.handler;public class GlobalExceptionHandler {
+package validation.interceptor.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import validation.interceptor.exception.AuthException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity authException() {
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 }
