2449763b71f79efe8cd6c3e352b6a9e064101609
meohin <meohin.meo@gmail.com>
2022-01-07 18:06:11 +0900

    Add codition public or private

--- Part3/src/chapter6/src/main/java/validation/interceptor/interceptor/AuthInterceptor.java
+++ Part3/src/chapter6/src/main/java/validation/interceptor/interceptor/AuthInterceptor.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 import validation.interceptor.annotation.Auth;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 @Slf4j
 @Component
@@ -19,11 +21,25 @@ public class AuthInterceptor implements HandlerInterceptor {
 
         String url = request.getRequestURI();
 
+        URI uri = UriComponentsBuilder.fromUriString(request.getRequestURI()).build().toUri();
+
         log.info("request url: {}", url);
 //        권한 체크
         boolean hasAnnotation = checkAnnotation(handler, Auth.class);
         log.info("has annotation: {}", hasAnnotation);
 
+//        조건
+//        나의 서버는 모두 public 으로 동작한다.
+//        단, Auth 권한을 가진 요청에 대해서는 private 으로 동작. 예를들면 세션, 쿠키,request parameter 의 특정값 등의 정책
+        if (hasAnnotation) {
+//            권한 체크
+            String query = uri.getQuery();
+            log.info("query: {}", query);
+            if (query.equals("name = steve")) {
+                return true;
+            }
+            return false;
+        }
 
         return true;       // true 일때만 Interceptor 넘어서서 logic 을 타게 된다.
     }
