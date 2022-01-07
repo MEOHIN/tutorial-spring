be646056f04b9a7cd37c71f398bc59a7f6a4ce7f
meohin <meohin.meo@gmail.com>
2022-01-07 17:48:53 +0900

    Implemnet Interceptor configuration

---
+++ Part3/src/chapter6/src/main/java/validation/interceptor/config/MVCConfig.java
@@ -0,0 +1,20 @@
+package validation.interceptor.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import validation.interceptor.interceptor.AuthInterceptor;
+
+@Configuration
+@RequiredArgsConstructor        // final 로 선언된 객체를 생성자에서 주입받을 수 있도록 해줌
+public class MVCConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(authInterceptor);
+    }
+}
