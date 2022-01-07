c85f9da570e047f0a06a8eedc5248820954e2a33
meohin <meohin.meo@gmail.com>
2022-01-07 17:39:50 +0900

    Implement interceptor to check authority

---
+++ Part3/src/chapter6/src/main/java/validation/interceptor/interceptor/AuthInterceptor.java
@@ -0,0 +1,44 @@
+package validation.interceptor.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+    @Override
+//    이 때 HttpServletRequest 가 response body 를 읽으면, 손실됨
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String url = request.getRequestURI();
+
+        log.info("request url: {}", url);
+
+        return false;
+    }
+
+//    annotation 유무 확인
+   private boolean checkAnnotation(Object handler, Class clazz) {
+
+//        resource 에 대한 요청일 땐, 권한 확인없이 무조건 통과: 예를들면, javascript, html
+        if (handler instanceof ResourceHttpRequestHandler) {
+            return true;
+        }
+
+//        annotation check
+       HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+        if (null != handlerMethod.getMethodAnnotation(clazz) || null != handlerMethod.getBeanType().getAnnotation(clazz)) {
+//            Auth annotation 이 있을 때 true;
+            return true;
+        }
+
+        return false;
+    }
+}
