dad99ee4a10c7a271a7c3a694da20f2dfdc28759
meohin <meohin.meo@gmail.com>
2022-01-10 17:57:37 +0900

    Make project about server to server connection

---
+++ Part3/src/chapter7/src/test/java/serverToServer/Chapter7ApplicationTests.java
@@ -0,0 +1,13 @@
+package serverToServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
