d15480255d40ddcdb207dcc547d19d18e29189c2
meohin <meohin.meo@gmail.com>
2022-01-12 14:10:33 +0900

    Make server offer data

---
+++ Part3/src/chapter7/src/server/src/main/java/server/dto/ServerUser.java
@@ -0,0 +1,13 @@
+package server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServerUser {
+    private String name;
+    private int age;
+}
