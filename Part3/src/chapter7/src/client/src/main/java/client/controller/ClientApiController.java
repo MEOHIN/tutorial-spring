6e37a3843031e9afe89eca7ea691dd5a90c5de10
meohin <meohin.meo@gmail.com>
2022-01-12 14:04:45 +0900

    Use ClientUserResponse is made instead of String

--- Part3/src/chapter7/src/client/src/main/java/client/controller/ClientApiController.java
+++ Part3/src/chapter7/src/client/src/main/java/client/controller/ClientApiController.java
@@ -1,5 +1,6 @@
 package client.controller;
 
+import client.dto.ClientUserResponse;
 import client.service.RestTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +18,8 @@ public class ClientApiController {
         this.restTemplateService = restTemplateService;
     }
 
-    @GetMapping("")
-    public String getHello () {
+    @GetMapping("hello")
+    public ClientUserResponse getHello () {
         return restTemplateService.hello();
     }
 }
--- Part3/src/chapter7/src/client/src/main/java/client/service/RestTemplateService.java
+++ Part3/src/chapter7/src/client/src/main/java/client/service/RestTemplateService.java
@@ -1,5 +1,7 @@
 package client.service;
 
+import ch.qos.logback.core.net.server.Client;
+import client.dto.ClientUserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,7 +17,7 @@ public class RestTemplateService {
 
 //    response 받기
 
-    public String hello() {
+    public ClientUserResponse hello() {
 //        어떻게 통신하냐?: client 가 되어야 하기 때문에 RestTemplate 사용
 //        URL 만들기
         URI uri = UriComponentsBuilder
@@ -31,7 +33,7 @@ public class RestTemplateService {
 //        Rest Template 만들기
         RestTemplate restTemplate = new RestTemplate();
 //        getForObject 의 get 은 가져오겠다는 뜻이 아니라 Http 메소두의 GET
-        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);   // getForObject 가 실행되는 순간 client 에서 http(서버)로 붙는 순간
+        ResponseEntity<ClientUserResponse> result = restTemplate.getForEntity(uri, ClientUserResponse.class);   // getForObject 가 실행되는 순간 client 에서 http(서버)로 붙는 순간
 
         System.out.println(result.getStatusCode());
         System.out.println(result.getBody());
