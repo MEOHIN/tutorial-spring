e2b840df8c744893b0da95d59a4db42978e9ce2b
meohin <meohin.meo@gmail.com>
2022-01-12 14:54:45 +0900

    Make client call post

--- Part3/src/chapter7/src/client/src/main/java/client/controller/ClientApiController.java
+++ Part3/src/chapter7/src/client/src/main/java/client/controller/ClientApiController.java
@@ -20,6 +20,6 @@ public class ClientApiController {
 
     @GetMapping("/hello")
     public UserResponse getHello () {
-        return restTemplateService.hello();
+        return restTemplateService.post();
     }
 }
