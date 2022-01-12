2e43a8021e9d2c715541f470a3fc7f9e8a065786
meohin <meohin.meo@gmail.com>
2022-01-12 17:50:39 +0900

    Implement connecting with naver open API

--- Part3/src/chapter7/src/server/src/main/java/server/controller/ServerApiController.java
+++ Part3/src/chapter7/src/server/src/main/java/server/controller/ServerApiController.java
@@ -2,14 +2,58 @@ package server.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import server.dto.Req;
 import server.dto.ServerUser;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+//        https://openapi.naver.com
+//        /v1/search/local.json
+//        ?query=%EC%A3%BC%EC%8B%9D
+//        &display=10
+//        &start=1
+//        &sort=random
+    @GetMapping("/naver")
+    public String naver(){
+
+        String query = "갈비집";
+        String encode = Base64.getEncoder().encodeToString(query.getBytes(StandardCharsets.UTF_8));
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", encode)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode()
+                .build()
+                .toUri();
+
+        log.info("url : {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "ZlEvjSz5ieDgX2yt3wtF")
+                .header("X-Naver-Client-Secret", "l6JxGtGIgn")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+        return result.getBody();
+    }
 
     @GetMapping("/hello")
     public ServerUser hello(@RequestParam String name, @RequestParam int age) {
