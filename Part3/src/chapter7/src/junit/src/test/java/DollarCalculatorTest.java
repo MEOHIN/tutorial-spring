57e9fb374da9a3477398ddc9537ac6872abd60fa
meohin <meohin.meo@gmail.com>
2022-01-13 14:44:24 +0900

    Add mock test and mock-up

--- Part3/src/chapter7/src/junit/src/test/java/DollarCalculatorTest.java
+++ Part3/src/chapter7/src/junit/src/test/java/DollarCalculatorTest.java
@@ -2,9 +2,24 @@ import calculator.Calculator;
 import calculator.DollarCalculator;
 import calculator.MarketAPI;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DollarCalculatorTest {
+
+//    모킹 처리
+    @Mock
+    public MarketAPI marketApi;
+
+    @BeforeEach
+    public void init() {
+        Mockito.lenient().when(marketApi.connect()).thenReturn(3000);
+    }
 
     @Test
     public void testHello() {
@@ -23,6 +38,21 @@ public class DollarCalculatorTest {
         System.out.println(calculator.sum(10, 10));
 
         Assertions.assertEquals(22000, calculator.sum(10, 10));
+        Assertions.assertEquals(0, calculator.minus(10, 10));
+
+    }
+
+    @Test
+    public void mockTest() {
+
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        System.out.println(calculator.sum(10, 10));
+
+        Assertions.assertEquals(60000, calculator.sum(10, 10));
         Assertions.assertEquals(0, calculator.minus(10, 10));
 
     }
