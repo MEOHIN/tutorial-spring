712025602a5ea5d27746d3637325aef4f27bc6d2
meohin <meohin.meo@gmail.com>
2022-01-13 14:33:26 +0900

    Remove constructor from Main class

--- Part3/src/chapter7/src/junit/src/main/java/calculator/Main.java
+++ Part3/src/chapter7/src/junit/src/main/java/calculator/Main.java
@@ -3,15 +3,5 @@ package calculator;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello JUnit");
-
-        MarketAPI marketApi = new MarketAPI();
-
-        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
-        dollarCalculator.init();
-
-        Calculator calculator = new Calculator(dollarCalculator);
-
-        System.out.println(calculator.sum(10, 10));
-
     }
 }
