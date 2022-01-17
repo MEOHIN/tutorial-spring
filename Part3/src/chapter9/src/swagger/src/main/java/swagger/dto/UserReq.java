2aa7c29db96731b69b6b49714b8351771cc3ff35
meohin <meohin.meo@gmail.com>
2022-01-17 18:10:44 +0900

    Improve user convenience

--- Part3/src/chapter9/src/swagger/src/main/java/swagger/dto/UserReq.java
+++ Part3/src/chapter9/src/swagger/src/main/java/swagger/dto/UserReq.java
@@ -1,6 +1,7 @@
 package swagger.dto;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserReq {
 
+    @ApiModelProperty(value = "사용자의 이름", example = "steve", required = true)
     private String name;
 
+    @ApiModelProperty(value = "사용자의 나이", example = "10", required = true)
     private int age;
 }
--- Part3/src/chapter9/src/swagger/src/main/java/swagger/dto/UserRes.java
+++ Part3/src/chapter9/src/swagger/src/main/java/swagger/dto/UserRes.java
@@ -1,5 +1,6 @@
 package swagger.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserRes {
 
+    @ApiModelProperty(value = "사용자의 이름", example = "steve", required = true)
     private String name;
 
+    @ApiModelProperty(value = "사용자의 나이", example = "10", required = true)
     private int age;
 }
