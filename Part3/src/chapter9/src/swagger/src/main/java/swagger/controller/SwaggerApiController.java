7e87061651561e9b13e2147e02f88a7bdd5c734c
meohin <meohin.meo@gmail.com>
2022-01-19 17:59:01 +0900

    Make show error message of Get

--- Part3/src/chapter9/src/swagger/src/main/java/swagger/controller/SwaggerApiController.java
+++ Part3/src/chapter9/src/swagger/src/main/java/swagger/controller/SwaggerApiController.java
@@ -3,6 +3,7 @@ package swagger.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.*;
 import swagger.dto.UserReq;
 import swagger.dto.UserRes;
@@ -27,6 +28,7 @@ public class SwaggerApiController {
         return x+y;
     }
 
+    @ApiResponse(code = 502, message = "사용자의 나이가 10살 이하일 때")
     @ApiOperation(value = "사용자의 이름과 나이를 리턴하는 메소드")
     @GetMapping("/user")
     public UserRes user(UserReq userReq) {
