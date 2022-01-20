60efa286381c966a27b624f41b46330859bdb7b1
meohin <meohin.meo@gmail.com>
2022-01-20 19:40:23 +0900

    Implement project for JPA

---
+++ Part4/chapter1/src/test/java/jpa/bookmaker/BookmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.bookmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
