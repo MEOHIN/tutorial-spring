bb6c5d2d5082b0d8ba869a1e85ac1369fa692774
meohin <meohin.meo@gmail.com>
2022-01-21 17:49:21 +0900

    Declare an object as an entity and create a primary key

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/User.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/User.java
@@ -2,6 +2,9 @@ package jpa.bookmaker.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 //@Getter
@@ -13,12 +16,19 @@ import java.time.LocalDateTime;
 //@EqualsAndHashCode          // equals 메소드와 해시코드 메소드를 오버라이드
 @Data                       // @Data = @Getter + @Setter + @ToString + @EqualsAndHashCode -> entity 객체를 사용하면서 가장많이 사용하는 어노테이션
 @Builder                    // @AllArgsConstructor 처럼 객체를 생성하고 필드값을 주입해주는데, builder 형식
+@Entity                     // 객체를 entity 선언. 결국 entity 는 DB 테이블과 연결되는 JAVA 객체다. entity 는 반드시 Primary key 가 필요.
 public class User {
+
+    @Id     // primary key
+    @GeneratedValue     // 순차적으로 데이터 증가
+    private long id;
+
     @NonNull    // 필수값 지정
     private String name;
 
     @NonNull
     private String email;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
