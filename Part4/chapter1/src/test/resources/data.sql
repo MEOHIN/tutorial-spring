6683b21fa0599b95d8bf1022f6199b5de0089932
meohin <meohin.meo@gmail.com>
2022-01-25 17:10:32 +0900

    Fix typo

--- Part4/chapter1/src/test/resources/data.sql
+++ Part4/chapter1/src/test/resources/data.sql
@@ -1,4 +1,4 @@
-call next value for hibernate_sequance; -- id 값을 증가시킴
+call next value for hibernate_sequence; -- id 값을 증가시킴
 insert into user ('id', 'name', 'email', 'created_at', 'updated_at') values (1, 'martin', 'martin@fastcampus.com', now(), now());
 
 call next value for hibernate_sequance; -- id 값을 증가시킴
