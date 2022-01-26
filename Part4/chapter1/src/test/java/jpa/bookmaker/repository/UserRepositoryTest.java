2b376b3dc41fa60196310bbda898418e221a8631
meohin <meohin.meo@gmail.com>
2022-01-26 18:14:43 +0900

    Test findAllById method

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
@@ -25,7 +25,8 @@ class UserRepositoryTest {
 //        System.out.println(">>>>" + userRepository.findAll());      // user 테이블에 있는 모든 데이터를 list 형식으로 가져오겠다
 
 //        보통 List 타입은 변수명으로 복수형을 사용한다.
-        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));    // 알파벳 역순 출력
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));    // 알파벳 역순 출력
+        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 2L, 5L));    // user 객체 long 타입의 ID 중 해당 ID만 출력
 
         users.forEach(System.out::println);
     }
