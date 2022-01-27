65333de2b7980a59f55df0cf9721f665d4365578
meohin <meohin.meo@gmail.com>
2022-01-27 17:47:31 +0900

    Add Flush sample

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
@@ -26,7 +26,7 @@ class UserRepositoryTest {
 //        userRepository.findAll().forEach(System.out::println);
 //        System.out.println(">>>>" + userRepository.findAll());      // user 테이블에 있는 모든 데이터를 list 형식으로 가져오겠다
 
-///**        보통 List 타입은 변수명으로 복수형을 사용한다. */
+//        /** 보통 List 타입은 변수명으로 복수형을 사용한다. */
 //        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));    // 알파벳 역순 출력
 //        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 2L, 5L));    // user 객체 long 타입의 ID 중 해당 ID만 출력
 //
@@ -42,11 +42,16 @@ class UserRepositoryTest {
 
 //        users.forEach(System.out::println);
 
-///**        getOne 은 entity 에 대해서 lazy 한 로딩을 지원 */
+//        /** getOne 은 entity 에 대해서 lazy 한 로딩을 지원 */
 //        User user = userRepository.getOne(1L);
 
-        User user = userRepository.findById(1L).orElse(null);
+//        User user = userRepository.findById(1L).orElse(null);
 
-        System.out.println(user);
+//        System.out.println(user);
+
+        /** Flush 는 DB 반영 시점에 영향은 주지만, log 에서는 변화를 찾아볼 수 없다.    */
+        userRepository.saveAndFlush(new User("new martin", "newmartin@fastcampus.com"));
+
+        userRepository.findAll().forEach(System.out::println);
     }
 }
