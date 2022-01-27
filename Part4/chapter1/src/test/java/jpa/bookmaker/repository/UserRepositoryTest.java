a90525944eca9618b34bfd476de5fc82b84788c9
meohin <meohin.meo@gmail.com>
2022-01-27 18:19:06 +0900

    Add deleteAllInBatch sample

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
@@ -61,8 +61,8 @@ class UserRepositoryTest {
 //        System.out.println(exist);
 //        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));  // 내가 작성한 select query 이외에 또다른 select query 가 생성됨
 //        userRepository.deleteById(1L);
-        userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(1L, 3L)));   // entity argument를 주어도 각각의 entity가 존재하는지 select 하고 각각 delete query 를 던짐
-
+//        userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(1L, 3L)));   // entity argument 를 주어도 각각의 entity 가 존재하는지 select 하고 각각 delete query 를 던짐
+        userRepository.deleteAllInBatch(userRepository.findAllById(Lists.newArrayList(1L, 3L)));   // 실제 delete query 를 한 번만 실행하고 select 도 하지 않기때문에 성능 이슈를 해결
         userRepository.findAll().forEach(System.out::println);
     }
 }
