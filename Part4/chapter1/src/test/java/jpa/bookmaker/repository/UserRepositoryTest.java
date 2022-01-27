83a451ac459130aa074156bcd4578716e0456a7c
meohin <meohin.meo@gmail.com>
2022-01-27 17:41:46 +0900

    Add orElse sample

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
@@ -19,14 +19,14 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    @Transactional  // Session 유지
+//    @Transactional  // Session 유지
     void crud() {   // create read update delete
 //        userRepository.save(new User());    // NoArgsConstructor 를 이용해서 생성하고 그것을 table 에 저장
 //
 //        userRepository.findAll().forEach(System.out::println);
 //        System.out.println(">>>>" + userRepository.findAll());      // user 테이블에 있는 모든 데이터를 list 형식으로 가져오겠다
 
-//        보통 List 타입은 변수명으로 복수형을 사용한다.
+///**        보통 List 타입은 변수명으로 복수형을 사용한다. */
 //        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));    // 알파벳 역순 출력
 //        List<User> users = userRepository.findAllById(Lists.newArrayList(1L, 2L, 5L));    // user 객체 long 타입의 ID 중 해당 ID만 출력
 //
@@ -42,7 +42,10 @@ class UserRepositoryTest {
 
 //        users.forEach(System.out::println);
 
-        User user = userRepository.getOne(1L);
+///**        getOne 은 entity 에 대해서 lazy 한 로딩을 지원 */
+//        User user = userRepository.getOne(1L);
+
+        User user = userRepository.findById(1L).orElse(null);
 
         System.out.println(user);
     }
