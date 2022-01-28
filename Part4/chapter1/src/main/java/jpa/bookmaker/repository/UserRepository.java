278b3ba1e41459d4dae1f03ead6c191b395fa8dc
meohin <meohin.meo@gmail.com>
2022-01-28 18:10:48 +0900

    Use set type as return type sample

--- Part4/chapter1/src/main/java/jpa/bookmaker/repository/UserRepository.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/repository/UserRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 //Spring Data JPA 라이브러리가 지원해주는 영역: 자바 인터페이스 선언 + jpa 인터페이스를 상속받음
 // JpaRepository<entity 타입, PK 타입>
 public interface UserRepository extends JpaRepository<User, Long> {
     /** query method    */
-    Optional<User> findByName(String name);   // 리턴받을 type 은 User 라는 entity 이고 User 단위 객체로 리턴하고, 이름 통해서 user 를 가져오는 method
+    Set<User> findByName(String name);   // 리턴받을 type 은 User 라는 entity 이고 User 단위 객체로 리턴하고, 이름 통해서 user 를 가져오는 method
 }
