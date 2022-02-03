15d58c80dcd0be85693d41b34dbe871227a7a186
meohin <meohin.meo@gmail.com>
2022-02-03 16:02:20 +0900

    Add test code of query method

--- Part4/chapter1/src/main/java/jpa/bookmaker/repository/UserRepository.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/repository/UserRepository.java
@@ -10,7 +10,8 @@ import java.util.Set;
 //Spring Data JPA 라이브러리가 지원해주는 영역: 자바 인터페이스 선언 + jpa 인터페이스를 상속받음
 // JpaRepository<entity 타입, PK 타입>
 public interface UserRepository extends JpaRepository<User, Long> {
-    /** query method : 본인이 사용하기에 가독성이 높은 걸로 사용    */
+    /** query method : 본인이 사용하기에 가독성이 높은 걸로 사용
+     * 필요에 따라 가독성을 위해 제공되는 접두어 사용가능 */
     Set<User> findByName(String name);   // 리턴받을 type 은 User 라는 entity 이고 User 단위 객체로 리턴하고, 이름 통해서 user 를 가져오는 method
 
     User findByEmail(String email);
