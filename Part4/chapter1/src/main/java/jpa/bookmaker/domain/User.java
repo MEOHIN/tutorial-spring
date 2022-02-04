fcf88760807fb365230d556fd7a538d153d26b43
meohin <meohin.meo@gmail.com>
2022-02-04 15:45:44 +0900

    Add documentation of @generatedValue annotation

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/User.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/User.java
@@ -18,8 +18,6 @@ import java.util.List;
 @Entity                     // 객체를 entity 선언. 결국 entity 는 DB 테이블과 연결되는 JAVA 객체다. entity 는 반드시 Primary key 가 필요.
 @Table(name = "user", indexes = {@Index(columnList = "name")}, uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})    // table 에 name, catalog, schema 은 default 로 자동 지정되지만 따로 지정할 수도 있음. index 또는 uniqueConstraints 와 같은 제약사항은 실제 DB 적용 것과 다를 수 있다; 따라서 이런식으로 entity 에 표기하지 않고 DB 에 직접 설정해서 사용하는 편이다.
 public class User {
-
-    @Id     // primary key
     /*  @GeneratedValue
     Long 타입의 값은 개발자가 직접 생성하는 것이 아닌 생성된 값(: table, sequence, identity, auto)을 쓰겠다는 의미
     IDENTITY: mysql DB 에서 많이 사용하는 전략: auto increase 값을 활용해서 이 generation 타입 제공. 트랜젝션이 종료되기전에 insert 문이 동작해서 id 값을 사전에 받아옴. 실제로 커밋되지 않고 로직이 종료된다 하더라도 DB 에서 가지고 있는 id 값을 증가시켜서 특정 id 값이 비는 현상이 발생하기도 함.
@@ -27,6 +25,7 @@ public class User {
     TABLE: DB 종류와 상관없이 id 를 관리하는 별도의 table 을 만들 그 table 에서 id 값을 추출해서 사용.
     AUTO: 지정하지 않으면 사용되는 default 값. 각 DB 에 맞게 값을 설정함.
      */
+    @Id     // primary key
     @GeneratedValue     // 순차적으로 데이터 증가.
     private Long id;
 
