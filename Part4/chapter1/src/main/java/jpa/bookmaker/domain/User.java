0089a6955facbd6e647db0d6ab5a29498008b58a
meohin <meohin.meo@gmail.com>
2022-02-04 17:52:31 +0900

    Fix typo

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/User.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/User.java
@@ -25,7 +25,7 @@ import java.util.List;
 public class User {
     /*  @GeneratedValue
     Long 타입의 값은 개발자가 직접 생성하는 것이 아닌 생성된 값(: table, sequence, identity, auto)을 쓰겠다는 의미
-    IDENTITY: mysql DB 에서 많이 사용하는 전략: auto increase 값을 활용해서 이 generation 타입 제공. 트랜젝션이 종료되기전에 insert 문이 동작해서 id 값을 사전에 받아옴. 실제로 커밋되지 않고 로직이 종료된다 하더라도 DB 에서 가지고 있는 id 값을 증가시켜서 특정 id 값이 비는 현상이 발생하기도 함.
+    IDENTITY: mysql DB 에서 많이 사용하는 전략: auto increment 값을 활용해서 이 generation 타입 제공. 트랜젝션이 종료되기전에 insert 문이 동작해서 id 값을 사전에 받아옴. 실제로 커밋되지 않고 로직이 종료된다 하더라도 DB 에서 가지고 있는 id 값을 증가시켜서 특정 id 값이 비는 현상이 발생하기도 함.
     SEQUENCE: oracle, h2 등에서 사용하는 전략: insert 구문이 실행될 때 sequence 로 부터 증가된 값을 받아서 실제 트렌젝션이 종료되는 시점에 id 를 채워서 query 함.
     TABLE: DB 종류와 상관없이 id 를 관리하는 별도의 table 을 만들 그 table 에서 id 값을 추출해서 사용.
     AUTO: 지정하지 않으면 사용되는 default 값. 각 DB 에 맞게 값을 설정함.
@@ -45,7 +45,7 @@ public class User {
 
     @Column(updatable = false)     // User 의 filed(=column) 에 속성을 지정하는 filed scope 의 annotation. 다양산 속성을 사용가능: 예를 들어 DB 의 컬럽과 Object 의 name 을 별도로 매핑할 땐, name 속성을 사용.
     private LocalDateTime createdAt;
-     private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt;
 
      /*
      Entity 는 Data 에 대한 객체이므로 DB 레코드 값을 그대로 반영.
