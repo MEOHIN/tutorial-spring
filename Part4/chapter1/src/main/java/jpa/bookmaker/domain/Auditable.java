7dccef58cd29930d82fed7d43ee1c4189220c24d
meohin <meohin.meo@gmail.com>
2022-02-09 18:09:13 +0900

    Add documentation for entity Listener

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/Auditable.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/Auditable.java
@@ -3,8 +3,8 @@ package jpa.bookmaker.domain;
 import java.time.LocalDateTime;
 
 /*
-createdAt, updatedAt 같은 것을 계속 사용하기 위해서
-createdAt, updatedAt 이 존재한다는 것을 알아야하기 때문에 만든 interface
+createdAt, updatedAt 처럼 로직 공통적인 부분에 대해서 listener 를 하나만 구현하고도 여러곳에서 사용할 수 있다.
+그러러면 createdAt, updatedAt 이 존재한다는 것을 알아야하기 때문에 만든 interface
  */
 public interface Auditable {
     LocalDateTime getCreatedAt();
--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/MyEntityListener.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/MyEntityListener.java
@@ -4,6 +4,9 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
+/*
+로직 공통적인 부분에 대해서 listener 를 하나만 구현하고도 여러곳에서 사용할 수 있다.
+ */
 public class MyEntityListener {
     @PrePersist
     public void prePersist(Object o) {  // Entity 객체에서는 파라미터를 받지 않아도 this 의 값이라서 Object 를 확인할 수 있지만, Listener 해당 Entity 를 받아서 처리해야하므로 Object 파라미터를 받아야 한다.
