cb7bfff99ef2d7837afa10c0d03547b5a5f46522
meohin <meohin.meo@gmail.com>
2022-02-09 18:13:02 +0900

    Create entity for history data

---
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
@@ -0,0 +1,29 @@
+package jpa.bookmaker.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class UserHistory {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long userId;
+
+    private String name;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
