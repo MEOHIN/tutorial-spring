b69d207ee7d5d7024baaace4a422c50dae7b64e1
meohin <meohin.meo@gmail.com>
2022-02-09 17:41:19 +0900

    Add Listener

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/Book.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/Book.java
@@ -3,9 +3,7 @@ package jpa.bookmaker.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity // entity 는 기본 생성자가 필요, pk 필요
@@ -21,4 +19,15 @@ public class Book {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
