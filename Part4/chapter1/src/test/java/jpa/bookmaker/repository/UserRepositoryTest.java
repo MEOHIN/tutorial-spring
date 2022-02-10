8d91340e6726c71b871b595a335e8efd87691bc7
meohin <meohin.meo@gmail.com>
2022-02-10 10:35:25 +0900

    Add test for UserRepository

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/UserRepositoryTest.java
@@ -21,6 +21,9 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserHistoryRepository userHistoryRepository;
+
     @Test
 //    @Transactional  // Session 유지
     void crud() {   // create read update delete
@@ -234,5 +237,21 @@ class UserRepositoryTest {
         userRepository.save(user);
 
         System.out.println("to-be: " + userRepository.findAll().get(0));
+    }
+
+    @Test
+    void userHistoryTest() {
+        User user = new User();
+
+        user.setEmail("martin-new@fastcampus.com");
+        user.setName("martin-new");
+
+        userRepository.save(user);
+
+        user.setName("martin-new-new");
+
+        userRepository.save(user);
+
+        userHistoryRepository.findAll().forEach(System.out::println);
     }
 }
