cf16785aec32f7e6b33e1bf88c1dd7c605956b38
meohin <meohin.meo@gmail.com>
2022-02-11 18:03:17 +0900

    Add missing declaration

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
@@ -4,14 +4,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+/*
+히스토리 데이터는 DB에 특정 데이터가 수정되면 해당 값의 복사본을 다른 테이블에 저장하는 경우가 있다.
+ */
 @Entity
 @NoArgsConstructor
 @Data
-public class UserHistory {
+@EntityListeners(value = MyEntityListener.class)
+public class UserHistory implements Auditable{
 
     @Id
     @GeneratedValue
