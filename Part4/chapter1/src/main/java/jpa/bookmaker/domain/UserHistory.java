d58029af4ad0276a589a02b1b6f5d5f14e3732bb
meohin <meohin.meo@gmail.com>
2022-02-14 18:11:02 +0900

    Add @CreatedDate and @LastModifiedDate to the data to be audited

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
@@ -2,6 +2,8 @@ package jpa.bookmaker.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Entity;
@@ -29,7 +31,9 @@ public class UserHistory implements Auditable{
 
     private String email;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
