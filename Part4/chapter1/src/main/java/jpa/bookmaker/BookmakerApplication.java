ff9fc8967f283613469e41fc94d038cce8dea632
meohin <meohin.meo@gmail.com>
2022-02-14 17:58:10 +0900

    Add auditing to use the default listener

--- Part4/chapter1/src/main/java/jpa/bookmaker/BookmakerApplication.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/BookmakerApplication.java
@@ -2,8 +2,10 @@ package jpa.bookmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // 기본 listener 를 사용하기위해 auditing 하겠다는 것을 추가
 public class BookmakerApplication {
 
     public static void main(String[] args) {
