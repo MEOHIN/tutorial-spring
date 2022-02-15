62358f549e8f56081a7a2e8fdb9319cbf96721d6
meohin <meohin.meo@gmail.com>
2022-02-15 18:08:27 +0900

    Add test for BookReviewInfoRepository

---
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookReviewInfoRepositoryTest.java
@@ -0,0 +1,27 @@
+package jpa.bookmaker.repository;
+
+import jpa.bookmaker.domain.BookReviewInfo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookReviewInfoRepositoryTest {
+    @Autowired  // 주입
+    private BookReviewInfoRepository bookReviewInfoRepository;
+
+    @Test
+    void crudTest() {
+        BookReviewInfo bookReviewInfo = new BookReviewInfo();
+        bookReviewInfo.setBookId(1L);
+        bookReviewInfo.setAverageReviewScore(4.5f);
+        bookReviewInfo.setReviewCount(2);
+
+        bookReviewInfoRepository.save(bookReviewInfo);
+
+        System.out.println(">>>> " + bookReviewInfoRepository.findAll());
+    }
+
+}
