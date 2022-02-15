f66c44c1785132f92ef95ccd5575f4ef9a9a3874
meohin <meohin.meo@gmail.com>
2022-02-15 18:27:20 +0900

    Remove unused import statement

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/Book.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/Book.java
@@ -1,21 +1,15 @@
 package jpa.bookmaker.domain;
 
-import jpa.bookmaker.domain.lietener.Auditable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity // entity 는 기본 생성자가 필요, pk 필요
 @NoArgsConstructor
 @Data   // getter setter 생성
-//@EntityListeners(value = AuditingEntityListener.class)
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class Book extends BaseEntity {
