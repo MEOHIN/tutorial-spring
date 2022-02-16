23545827eef750d2517fe4102a4a3f85b6789fbc
meohin <meohin.meo@gmail.com>
2022-02-16 17:37:01 +0900

    Add dependency

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookReviewInfoRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookReviewInfoRepositoryTest.java
@@ -11,6 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class BookReviewInfoRepositoryTest {
     @Autowired  // 주입
     private BookReviewInfoRepository bookReviewInfoRepository;
+    @Autowired  // 의존성 추가
+    private BookRepository bookRepository;
 
     @Test
     void crudTest() {
