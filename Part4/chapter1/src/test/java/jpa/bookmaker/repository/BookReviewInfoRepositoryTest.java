790aef1ed677f01ff4d64d61245aa9e0c6783365
meohin <meohin.meo@gmail.com>
2022-02-16 17:37:40 +0900

    Add test for CRUD

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookReviewInfoRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookReviewInfoRepositoryTest.java
@@ -1,5 +1,6 @@
 package jpa.bookmaker.repository;
 
+import jpa.bookmaker.domain.Book;
 import jpa.bookmaker.domain.BookReviewInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,36 @@ class BookReviewInfoRepositoryTest {
         bookReviewInfoRepository.save(bookReviewInfo);
 
         System.out.println(">>>> " + bookReviewInfoRepository.findAll());
+    }
+
+    @Test
+    void crudTest2() {
+        Book book = new Book();
+        book.setName("Jpa 초격차 패키지");
+        book.setAuthorId(1L);
+        book.setPublisherId(1L);
+
+        bookRepository.save(book);
+
+        System.out.println(">>>> " + bookRepository.findAll());
+
+        BookReviewInfo bookReviewInfo = new BookReviewInfo();
+        bookReviewInfo.setBookId(1L);
+        bookReviewInfo.setAverageReviewScore(4.5f);
+        bookReviewInfo.setReviewCount(2);
+
+        bookReviewInfoRepository.save(bookReviewInfo);
+
+        System.out.println(">>>> " + bookReviewInfoRepository.findAll());
+
+        Book result = bookRepository.findById(
+                bookReviewInfoRepository
+                        .findById(1L)
+                        .orElseThrow(RuntimeException::new)
+                        .getBookId()
+        ).orElseThrow(RuntimeException::new);
+
+        System.out.println(">>>> " + result);
     }
 
 }
