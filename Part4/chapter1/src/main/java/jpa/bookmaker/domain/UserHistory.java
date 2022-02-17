926f574494282e4c426a9af99747dc630493f968
meohin <meohin.meo@gmail.com>
2022-02-17 16:58:07 +0900

    Remove unused code

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/UserHistory.java
@@ -1,16 +1,11 @@
 package jpa.bookmaker.domain;
 
-import jpa.bookmaker.domain.lietener.Auditable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 /*
 히스토리 데이터는 DB에 특정 데이터가 수정되면 해당 값의 복사본을 다른 테이블에 저장하는 경우가 있다.
@@ -32,10 +27,4 @@ public class UserHistory extends BaseEntity {
     private String name;
 
     private String email;
-
-//    @CreatedDate
-//    private LocalDateTime createdAt;
-//
-//    @LastModifiedDate
-//    private LocalDateTime updatedAt;
 }
