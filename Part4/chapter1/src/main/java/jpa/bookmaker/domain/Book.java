0b6e8458124ce32e480980410f931085e448e0b8
meohin <meohin.meo@gmail.com>
2022-02-18 18:11:51 +0900

    Set book relationship with Review and publisher

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/Book.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/Book.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity // entity 는 기본 생성자가 필요, pk 필요
 @NoArgsConstructor
@@ -30,4 +32,12 @@ public class Book extends BaseEntity {
     @OneToOne(mappedBy = "book")  // entity relational 을 사용하는 경우 toString 메소드가 순환참조가 걸리게 된다.
     @ToString.Exclude   // 따라서 대부분 relational 을 단반향으로 걸거나 toString 에서 제외하는 처리기 필요
     private BookReviewInfo bookReviewInfo;  // book 에서 bookReviewInfo 데이터를 접근
+
+    @OneToMany
+    @JoinColumn(name = "book_id")
+    private List<Review> reviews = new ArrayList<>();
+
+    @ManyToOne
+    @ToString.Exclude
+    private Publisher publisher;
 }
