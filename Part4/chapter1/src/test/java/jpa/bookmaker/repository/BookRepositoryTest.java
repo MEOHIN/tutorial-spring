8e4ccb2a148ade516a33cfc0250f60b0bd5298bc
meohin <meohin.meo@gmail.com>
2022-02-21 18:12:12 +0900

    Add test for check that the data is working

--- Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookRepositoryTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/repository/BookRepositoryTest.java
@@ -1,6 +1,9 @@
 package jpa.bookmaker.repository;
 
 import jpa.bookmaker.domain.Book;
+import jpa.bookmaker.domain.Publisher;
+import jpa.bookmaker.domain.Review;
+import jpa.bookmaker.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,15 @@ public class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void bookTest() {
         Book book = new Book();
@@ -20,5 +32,50 @@ public class BookRepositoryTest {
         bookRepository.save(book);
 
         System.out.println(bookRepository.findAll());
+    }
+
+    @Test
+    void bookRelationTest() {
+        givenBookAndReview();
+
+        User user = userRepository.findByEmail("martin@fastcampus.com");
+
+        System.out.println("Review: " + user.getReviews());
+        System.out.println("Book: " + user.getReviews().get(0).getBook());
+        System.out.println("Publisher: " + user.getReviews().get(0).getBook().getPublisher());
+    }
+
+    private void givenBookAndReview() {
+        givenReview(givenUser(), givenBook(givenPublisher()));
+    }
+
+    private User givenUser() {
+        return userRepository.findByEmail("martin@fastcampus.com");
+    }
+
+    private void givenReview(User user, Book book) {
+        Review review = new Review();
+        review.setTitle("내 인생을 바꾼 책");
+        review.setContent("너무너무 재미있고 즐거운 책이었어요.");
+        review.setScore(5.0f);
+        review.setUser(user);
+        review.setBook(book);
+
+        reviewRepository.save(review);
+    }
+
+    private Book givenBook(Publisher publisher) {
+        Book book = new Book();
+        book.setName("JPA 초 격차 패키지");
+        book.setPublisher(publisher);
+
+        return bookRepository.save(book);
+    }
+
+    private Publisher givenPublisher() {
+        Publisher publisher = new Publisher();
+        publisher.setName("패스트캠퍼스");
+
+        return publisherRepository.save(publisher);
     }
 }
