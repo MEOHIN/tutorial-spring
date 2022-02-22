1547fb26fcb487a20eb318ba4c525509e31b32e2
meohin <meohin.meo@gmail.com>
2022-02-22 11:59:49 +0900

    Add document for many-to-many

--- Part4/chapter1/src/main/java/jpa/bookmaker/domain/Author.java
+++ Part4/chapter1/src/main/java/jpa/bookmaker/domain/Author.java
@@ -29,6 +29,7 @@ public class Author extends BaseEntity{
     /*
     many to many 는 PK 를 정하기 어려워서 중간테이블을 생성하고 author_id 와 books_id 를 서로 매핑하게 된다.
     따라서 one to many 관계에서는 중간 테이블을 제거하고 직접 참조하도록 했었지만, many to many 는 중간테이블 없이 직접참조할 수 없다.
+    현업에서는 many-to-many 는 아주 특별한 상황이 아니라면 거의 사용하지 않는다.
      */
     @ManyToMany
     @ToString.Exclude
