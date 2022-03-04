c1e322dd325fe33ac0458aa8321541f0aa4b054e
meohin <meohin.meo@gmail.com>
2022-03-04 17:47:42 +0900

    Fix IllegalArgumentException, JPA metamodel must not be empty

--- Part4/chapter1/src/test/java/jpa/bookmaker/controller/HelloWorldControllerTest.java
+++ Part4/chapter1/src/test/java/jpa/bookmaker/controller/HelloWorldControllerTest.java
@@ -3,6 +3,8 @@ package jpa.bookmaker.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -10,7 +12,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+
+@WebMvcTest     // = 슬라이스 테스트: 전체 Spring Context 를 로딩하지 않고 Web Controller 에 대한 일부 Bean 들을 로딩해서 테스트함 -> JPA 옵션에서 예외 발생
+@MockBean(JpaMetamodelMappingContext.class)   // 슬라이스 테스트로 인한 문제 해결방법
 class HelloWorldControllerTest {
 
     @Autowired
