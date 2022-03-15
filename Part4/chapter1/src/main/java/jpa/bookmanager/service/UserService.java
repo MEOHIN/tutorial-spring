37f55c2464b7562f20d61c930f3ab82a7f479d44
meohin <meohin.meo@gmail.com>
2022-03-15 18:13:47 +0900

    Add code for managed as entity lifecycle

--- Part4/chapter1/src/main/java/jpa/bookmanager/service/UserService.java
+++ Part4/chapter1/src/main/java/jpa/bookmanager/service/UserService.java
@@ -1,12 +1,17 @@
 package jpa.bookmanager.service;
 
 import jpa.bookmanager.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Transactional
     public void put() {
@@ -14,5 +19,10 @@ public class UserService {
         User user = new User();
         user.setName("newUser");
         user.setEmail("newUser@fastcampus.com");
+
+//        영속화
+        entityManager.persist(user);
+
+        user.setName("newUserAfterPersist");    // managed 의 경우 save 을 실행하지 않더라도 Transaction 이 종료되는 시점에 update 쿼리가 실행되서 데이터가 반영된다.
     }
 }
