9a81dcbc32ef6125be558d27fbc275e87c80c0c0
meohin <meohin.meo@gmail.com>
2022-03-17 17:10:11 +0900

    Add test for book service

---
+++ Part4/chapter1/src/test/java/jpa/bookmanager/service/BookServiceTest.java
@@ -0,0 +1,29 @@
+package jpa.bookmanager.service;
+
+import jpa.bookmanager.domain.Author;
+import jpa.bookmanager.repository.AuthorRepository;
+import jpa.bookmanager.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Test
+    void transactionTest() {
+        bookService.putBookAndAuthor();
+
+        System.out.println("books : " + bookRepository.findAll());
+        System.out.println("authors : " + authorRepository.findAll());
+    }
+}
