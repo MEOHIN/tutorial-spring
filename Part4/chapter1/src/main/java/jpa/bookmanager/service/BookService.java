323ee7bfbe40761b492704485253c743f22dffcf
meohin <meohin.meo@gmail.com>
2022-03-17 17:23:50 +0900

    Add transaction

--- Part4/chapter1/src/main/java/jpa/bookmanager/service/BookService.java
+++ Part4/chapter1/src/main/java/jpa/bookmanager/service/BookService.java
@@ -6,6 +6,7 @@ import jpa.bookmanager.repository.AuthorRepository;
 import jpa.bookmanager.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor    // @Autowired 로 필드 주입하는 대신 @RequiredArgsConstructor 사용을 권장
@@ -13,6 +14,7 @@ public class BookService {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
 
+    @Transactional
     public void putBookAndAuthor() {
         Book book = new Book();
         book.setName("JPA 시작하기");
