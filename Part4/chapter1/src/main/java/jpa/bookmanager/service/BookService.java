a98338d2785adc2c7258f2a72a5ffab74b82deb7
meohin <meohin.meo@gmail.com>
2022-03-18 18:03:10 +0900

    Add for REPEATABLE_READ test

--- Part4/chapter1/src/main/java/jpa/bookmanager/service/BookService.java
+++ Part4/chapter1/src/main/java/jpa/bookmanager/service/BookService.java
@@ -9,11 +9,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
 @Service
 @RequiredArgsConstructor    // @Autowired 로 필드 주입하는 대신 @RequiredArgsConstructor 사용을 권장
 public class BookService {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final EntityManager entityManager;
 
     @Transactional
     public void putBookAndAuthor() {
@@ -35,8 +38,12 @@ public class BookService {
         System.out.println(">>> " + bookRepository.findById(id));
         System.out.println(">>> " + bookRepository.findAll());
 
+        entityManager.clear();
+
         System.out.println(">>> " + bookRepository.findById(id));
         System.out.println(">>> " + bookRepository.findAll());
+
+        entityManager.clear();
 
 //        Book book = bookRepository.findById(id).get();  // book 이라는 entity 는 uncommitted 데이터를 가지고 있음
 //        book.setName("바뀔까?");
