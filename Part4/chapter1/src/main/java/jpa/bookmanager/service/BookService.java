c9829890919d98917cdc9b6330f44448d19f03d2
meohin <meohin.meo@gmail.com>
2022-03-21 18:07:58 +0900

    Add author service

--- Part4/chapter1/src/main/java/jpa/bookmanager/service/BookService.java
+++ Part4/chapter1/src/main/java/jpa/bookmanager/service/BookService.java
@@ -7,6 +7,7 @@ import jpa.bookmanager.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -17,20 +18,16 @@ public class BookService {
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
     private final EntityManager entityManager;
+    private final AuthorService authorService;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED)
     public void putBookAndAuthor() {
         Book book = new Book();
         book.setName("JPA 시작하기");
 
         bookRepository.save(book);
 
-        Author author = new Author();
-        author.setName("martin");
-
-        authorRepository.save(author);
-
-        throw new RuntimeException("오류가 발생해서 commit 이 발생하지 않았습니다.");    // rollback
+        authorService.putAuthor();
     }
 
     @Transactional(isolation = Isolation.READ_COMMITTED)
