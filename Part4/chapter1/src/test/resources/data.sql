08325ba5f5e4ba36e9ec32e76eb6d6bd2c12772f
meohin <meohin.meo@gmail.com>
2022-03-25 17:56:06 +0900

    Add data, including delete setting

--- Part4/chapter1/src/test/resources/data.sql
+++ Part4/chapter1/src/test/resources/data.sql
@@ -13,8 +13,10 @@ insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (4, 
 -- call next value for hibernate_sequence; -- id 값을 증가시킴
 insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (5, 'martin', 'martin@slowcampus.com', now(), now());
 
--- insert into publisher(`id`, `name`) values (1, '패스트캠퍼스');
+insert into publisher(`id`, `name`) values (1, '패스트캠퍼스');
 
--- insert into book(`id`, `name`, `publisher_id`) values (1, 'JPA 초격차패키지', 1);
+insert into book(`id`, `name`, `publisher_id`, `deleted`) values (1, 'JPA 초격차패키지', 1, false);
 
--- insert into book(`id`, `name`, `publisher_id`) values (2, 'Spring Security 초격차 패키지', 1);
+insert into book(`id`, `name`, `publisher_id`, `deleted`) values (2, 'Spring Security 초격차 패키지', 1, false);
+
+insert into book(`id`, `name`, `publisher_id`, `deleted`) values (3, 'Spring Security 초격차 패키지', 1, true);   -- delete
